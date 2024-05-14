91a2bd72bbfa39080ead7f41d1a66b7462c97c45
Dunai Valéria <dunai.valeria@gmail.com>
2024-05-14 21:49:35 +0200

    javítás

--- recipes_current_content.js
+++
@@ -1,5 +0,0 @@
-function currentcontent() {
-    <iframe src="./templates/pages/eloetelek.tpl.php" height="800" width="800"></iframe>
-}
-
-//"document.location.href='./templates/pages/<?=$url?>.tpl.php'"
--- service/contactok_lekerese.php
+++ service/contactok_lekerese.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=dailyrecipe;charset=utf8', 'root', '',
+    $db = new PDO('mysql:host=ftp.nethely.hu;dbname=dailyrecipe;charset=utf8', 'root', '',
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
     $sqlQuery = "select nev, datum, bejelentkezett_e, content from contact order by datum DESC";
